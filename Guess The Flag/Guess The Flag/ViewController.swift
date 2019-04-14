72a819a29004a96a7ce8d133b0ce911e162d2bfc
Reema <it.reema7@gmail.com>
2019-04-15 00:17:00 +0300

    Add files via upload

--- Guess The Flag/Guess The Flag/ViewController.swift
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
         //Border on the Flag
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -34,12 +36,38 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        //shuffle() That will automatically randomize the order of the countries in the array
+        countries.shuffle()
+        //This gives us a new integer property that will store whether it's flag 0, 1 or 2 that holds the correct answer
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-    }
         
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
+    
     
 
 
